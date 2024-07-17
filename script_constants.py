62849e0db4449dcddb78a8ae2057cd32a7150d11
Vinicid <vinicius.carvalho@nepen.org.br>
2024-07-17 17:23:50 -0300

    Add script constants file

---
+++ script_constants.py
@@ -0,0 +1,9 @@
+#===============================================================================
+#         Source Code Constants
+#===============================================================================
+
+STLINK_PATH = "C:\\Program Files\\STMicroelectronics\\STM32Cube\\STM32CubeProgrammer\\bin\\STM32_Programmer_CLI.exe"
+LOCK_DELAY = 1
+GENERATE_LOG: bool = True
+FW_PATH = "C:\\Firmwares\\tagvis\\fw\\src\\Projects\\LoRaWAN_End_Node\\STM32CubeIDE\Debug\\STM32CubeIDE.bin"
+ADDRESS_STLINK = 0x08000000
